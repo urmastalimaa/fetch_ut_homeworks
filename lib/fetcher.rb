acc8280bb16087bcbf49af7f6962deb90b8ceb3a
urmastalimaa <urmas7@gmail.com>
2018-03-23 13:39:47 +0200

    Add homework fetcher
    Fetch homework submission html and extract submission info.

---
+++ lib/fetcher.rb
@@ -0,0 +1,55 @@
+require 'faraday'
+require 'nokogiri'
+require 'submission'
+
+# Fetches information about all submissions for a single task from the course page
+class Fetcher
+  COURSE_NR = 1508
+  REMOTE_SUBMISSIONS_ROOT = "/course-#{COURSE_NR}/submissions/by_task".freeze
+  DOMAIN = 'https://courses.cs.ut.ee'.freeze
+
+  def initialize(logger, homework_nr, session_id)
+    @logger = logger
+    @homework_nr = homework_nr
+    @session_id = session_id
+    @remote_path = "#{REMOTE_SUBMISSIONS_ROOT}/#{homework_nr}"
+
+    @conn = Faraday.new(url: DOMAIN) do |faraday|
+      faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+    end
+  end
+
+  def submissions!
+    @logger.info "Fetching submission info for homework #{@homework_nr}"
+    extract_submission_hrefs(submissions_html)
+      .map(&Submission.method(:from_href))
+      .group_by(&:matricle_nr)
+      .map { |_, submissions| StudentSubmissions.new(submissions) }
+  end
+
+  def download_submission!(path)
+    @logger.info "Downloading submission #{path}"
+    request_from_courses(path).body
+  end
+
+  private
+
+  def request_from_courses(path)
+    @conn.get do |req|
+      req.url path
+      req.headers['Cookie'] = "COURSESSID=#{@session_id}"
+    end
+  end
+
+  def submissions_html
+    Nokogiri::HTML(request_from_courses(@remote_path).body)
+  end
+
+  def extract_submission_hrefs(html)
+    html
+      .css('tbody > tr')
+      .map { |r| r.css('a').first }
+      .compact
+      .map { |href| href.attributes['href'].value }
+  end
+end
---
+++ lib/submission.rb
@@ -0,0 +1,34 @@
+# Describes a single submission
+class Submission
+  attr_reader :remote_path, :matricle_nr, :filename
+
+  def self.from_href(href)
+    matricle_nr = href.match(/.*\/get\/\d\/([^.]*).*\//)[1]
+    original_filename = href.match(/[^\/]*$/)[0]
+    new(href, matricle_nr, original_filename)
+  end
+
+  def initialize(remote_path, matricle_nr, filename)
+    @remote_path = remote_path
+    @matricle_nr = matricle_nr
+    @filename = filename
+  end
+end
+
+# Describes all submissions of a student
+class StudentSubmissions
+  attr_reader :submission_count, :matricle_nr, :latest
+
+  def initialize(submissions)
+    @submissions = submissions
+    @matricle_nr = submissions.first.matricle_nr
+    @submission_count = submissions.length
+    @latest = @submissions.max_by(&:filename)
+  end
+
+  def to_s
+    "Student #{matricle_nr} submission: " \
+      "count: #{submission_count}, " \
+      "latest filename: #{latest.filename}"
+  end
+end
